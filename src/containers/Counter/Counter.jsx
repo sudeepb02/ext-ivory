89532728b41ce584ce772eb7aa418083c02ccbed
Sudeep Biswas <sudeepb02@gmail.com>
2024-08-11 03:02:02 +0530

    Add EAS attestation logic to extension popup

---
+++ src/containers/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Current count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/containers/Greetings/Greetings.jsx
+++ src/containers/Greetings/Greetings.jsx
@@ -3,14 +3,14 @@ import icon from '../../assets/img/icon-128.png';
 
 class GreetingComponent extends Component {
   state = {
-    name: 'dev',
+    name: 'Deepali',
   };
 
   render() {
     return (
       <div>
         <p>Hello, {this.state.name}!</p>
-        <img src={icon} alt="extension icon" />
+        {/* <img src={icon} alt="extension icon" /> */}
       </div>
     );
   }
