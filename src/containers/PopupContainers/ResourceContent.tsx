8dd4a67ddff0226e1ba8672e203ceaf0c948b08b
Sudeep Biswas <sudeepb02@gmail.com>
2024-08-11 18:06:08 +0530

    Add Subgraph data logic

--- src/containers/PopupContainers/ResourceContent.tsx
+++ src/containers/PopupContainers/ResourceContent.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-// import ResourceContents from './ResourceContent';
-
+import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 interface ResourceContent {
@@ -32,27 +30,11 @@ const GET_RESOURCE_CONTENTS = gql`
   }
 `;
 
-export const SiteStatus = () => {
-  // Set the url of the current page
-  const [currentUrl, setCurrentUrl] = useState('');
-  const [scamCount, setScamCount] = useState(0);
-
+// Create a React component to fetch and display data
+const ResourceContents: React.FC = () => {
   const { loading, error, data } = useQuery<GraphQLResponse>(
     GET_RESOURCE_CONTENTS
   );
-
-  const addScamCount = (count: number) => {
-    setScamCount(scamCount + count);
-    return '';
-  };
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0 && tabs[0].url) {
-        setCurrentUrl(tabs[0].url);
-      }
-    });
-  }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -60,13 +42,20 @@ export const SiteStatus = () => {
   return (
     <div>
       {data?.resourceContents.map((item) => (
-        <div key={item.id}>{addScamCount(Number(item.isScamCount))}</div>
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>
+            Content: <a href={item.content}>{item.content}</a>
+          </p>
+          <p>Content Hash: {item.contentHash}</p>
+          <p>Type: {item.type}</p>
+          <p>Is Scam Count: {item.isScamCount}</p>
+          <p>Not Scam Count: {item.notScamCount}</p>
+        </div>
       ))}
-
-      <h3>
-        Current URL: <p>{currentUrl}</p>
-        Scam Reports: <b>{scamCount}</b>
-      </h3>
+      import ReactDOM from 'react-dom';
     </div>
   );
 };
+
+export default ResourceContents;
